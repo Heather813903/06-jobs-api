f6dbe6a46b32ebe77e31404096c01f0f1b780315
Heather813903 <heathersmith9378@gmail.com>
2026-02-01 20:55:36 -0500

    Week 10 complete. CRUD and deploy prep

--- app.js
+++ app.js
@@ -16,15 +16,32 @@ const errorHandlerMiddleware = require("./middleware/error-handler");
 
 app.use(express.json());
 // extra packages
+const helmet = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
+
 
 // routes
 
 
 app.use("/api/v1/auth", authRouter)
 app.use("/api/v1/jobs", authenticateUser, jobsRouter)
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000, 
+    max: 100, 
+  })
+);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
 
 const port = process.env.PORT || 3000;
 
