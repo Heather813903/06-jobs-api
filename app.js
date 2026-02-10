479fb4a7d58c7a5a76f55c1d0ca62f4a5e77710b
Heather813903 <heathersmith9378@gmail.com>
2026-02-09 21:50:57 -0500

    Week 11: add client-side frontend and wire CRUD operations

--- app.js
+++ app.js
@@ -34,7 +34,12 @@ app.use(
     max: 100, 
   })
 );
+
+/*app.get("/", (req, res) => {
+  res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});*/
   
+app.use(express.static("public"));
 
 // routes
 
